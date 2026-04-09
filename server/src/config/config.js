4af9b3cd28bbe99c7e7794adc9c96f64df651fa9
Oki <biek.syaoki@gmail.com>
2026-04-09 23:19:58 +0700

    chore: initiation npm and setup sequelize database

---
+++ server/src/config/config.js
@@ -0,0 +1,26 @@
+require('dotenv').config(); // Biar bisa baca file .env
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME || 'root',
+    password: process.env.DB_PASSWORD || null,
+    database: process.env.DB_NAME || 'database_dev',
+    host: process.env.DB_HOST || '127.0.0.1',
+    dialect: process.env.DB_DIALECT || 'postgres',
+  },
+  test: {
+    username: process.env.DB_USERNAME || 'root',
+    password: process.env.DB_PASSWORD || null,
+    database: process.env.DB_NAME_TEST || 'database_test',
+    host: process.env.DB_HOST || '127.0.0.1',
+    dialect: process.env.DB_DIALECT || 'postgres',
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    logging: false, // Biar gak nyampah log di console production
+  },
+};
