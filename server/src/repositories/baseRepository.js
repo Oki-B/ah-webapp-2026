286a49727a31da4912e52362da0b66bfd590f035
Oki <biek.syaoki@gmail.com>
2026-04-14 18:35:05 +0700

    feat: implement user repository with unit tests

---
+++ server/src/repositories/baseRepository.js
@@ -0,0 +1,35 @@
+class BaseRepository {
+  constructor(model) {
+    this.model = model;
+  }
+
+  async findAll(options = {}) {
+    return this.model.findAll(options);
+  }
+
+  async findById(id, options = {}) {
+    return this.model.findByPk(id, options);
+  }
+
+  async findOne(options = {}) {
+    return this.model.findOne(options);
+  }
+
+  async create(data, options = {}) {
+    return this.model.create(data, options);
+  }
+
+  async update(id, data, options = {}) {
+    const record = await this.model.findByPk(id);
+    if (!record) return null;
+    return record.update(data, options);
+  }
+
+  async delete(id) {
+    const record = await this.model.findByPk(id);
+    if (!record) return null;
+    return record.destroy();
+  }
+}
+
+module.exports = BaseRepository;
